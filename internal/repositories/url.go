efb059efdd532c2b0c537bba0462ef482b5fb596
hamid <s.hamid.sajjadi@gmail.com>
2022-07-20 01:03:24 +0430

    feat: adds http handler and following routes - POST /shorten -> shorten the given URL - GET /:shortenURL -> redirects the shortened URL to the source

--- internal/repositories/url.go
+++ internal/repositories/url.go
@@ -12,7 +12,7 @@ type URLModel struct {
 type URLRepository interface {
 	GetOne(shortenedURL string) (url *URLModel, err error)
 	Save(sourceURL string, shortURL string) (url *URLModel, err error)
-	IncVisits(shortenedURL string) (url *URLModel, err error)
+	IncViews(shortenedURL string) (err error)
 }
 
 type InMemoryURLRepos struct {
@@ -22,6 +22,14 @@ type InMemoryURLRepos struct {
 	maxID          int32
 }
 
+func NewInMemoryRepo() URLRepository {
+	return &InMemoryURLRepos{
+		sourceToURL:    make(map[string]*URLModel),
+		shortenedToURL: make(map[string]*URLModel),
+		idToURL:        make(map[int32]*URLModel),
+		maxID:          0,
+	}
+}
 func (i InMemoryURLRepos) GetOne(shortenedURL string) (url *URLModel, err error) {
 	url, ok := i.shortenedToURL[shortenedURL]
 	if !ok {
--- main.go
+++ main.go
@@ -1,1 +1,35 @@
-package ushort
+package main
+
+import (
+	"fmt"
+	"github.com/HamidSajjadi/ushort/api"
+	"github.com/HamidSajjadi/ushort/internal/repositories"
+	"github.com/gin-gonic/gin"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func initialize() {
+	urlRepo := repositories.NewInMemoryRepo()
+	httpStub := gin.Default()
+
+	handler := api.New(httpStub, urlRepo)
+	handler.Run("localhost:9090")
+}
+func wait() {
+	signals := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		sig := <-signals
+		fmt.Println("Signal received: ", sig)
+		done <- true
+	}()
+	<-done
+}
+
+func main() {
+	initialize()
+	wait()
+}
